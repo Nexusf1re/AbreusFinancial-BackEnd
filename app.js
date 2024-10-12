09de4b11e8275381a8c2121c6de6893168529138
Nexusf1re <matteussantos30@hotmail.com>
2024-10-12 16:23:05 -0300

    some dependencies installed and basic configs made

--- app.js
+++ app.js
@@ -1,1 +1,14 @@
 const express = require('express');
+const cors = require('cors');
+const app = express();
+const PORT = 3000;
+
+app.use(cors());
+
+app.get('/', (request, response) => {
+    response.send("Get recebido, parabens Gabribri")
+});
+
+app.listen(PORT, () => {
+        console.log(`Server is running on http://localhost:${PORT}`)
+})

bc7d71ce0b0b33bf763292bb24dbe82e30e860ec
Matteus <matteussantos30@gmail.com>
2024-10-15 11:16:59 -0300

    Funções de cada rota documentadas

--- app.js
+++ app.js
@@ -26,4 +26,4 @@ app.get('/', (request, response) => {
 
 app.listen(PORT, () => {
         console.log(`Server is running on http://localhost:${PORT}`)
-})
+});
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -2,9 +2,9 @@ const express = require('express');
 const router = express.Router();
 //const authenticateToken = require('../middleware/authMiddleware');
 
+
 // Rota para validar o token
 router.get('/validate-token', (req, res) => {
-  // Se o token for válido, responde com status 200
   res.status(200).json({ message: 'Token válido' });
 });
 

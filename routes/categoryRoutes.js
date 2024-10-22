0937958400b66bc2270b008fce2f86aa55c07927
Matteus <matteussantos30@gmail.com>
2024-10-22 10:47:32 -0300

    Routes and queries updated based on DB changes

--- routes/categoryRoutes.js
+++ routes/categoryRoutes.js
@@ -9,15 +9,15 @@ const router = express.Router();
 //Rota para listar as categorias
 router.get("/list", (req, res) => {
 
-    const userId = req.query.userId;
-    if(!userId) {
-         return res.status(400).json({ message: "userId é obrigatório." });
+    const UserId = req.query.UserId;
+    if(!UserId) {
+         return res.status(400).json({ message: "UserId é obrigatório." });
     }
 
-    const query = 'SELECT * FROM category WHERE userId = ?';
+    const query = 'SELECT * FROM categories WHERE UserId = ?';
 
 
-    pool.query(query,[userId], (err, results) => {
+    pool.query(query,[UserId], (err, results) => {
         if (err) {
             console.error("Erro ao buscar os dados:", err);
             return res.status(500).send("Erro ao buscar os dados");
@@ -30,14 +30,14 @@ router.get("/list", (req, res) => {
 
 //Rota para cadastrar as categorias
 router.post("/register", (req, res) => {
-    const { userId, category, type } = req.body;
+    const { UserId, Category, Type } = req.body;
 
-    if (!userId || !category || !type) {
-        return res.status(400).json({ message: "userId, category e type são obrigatórios." });
+    if (!UserId || !Category || !Type) {
+        return res.status(400).json({ message: "UserId, Category e Type são obrigatórios." });
     }
 
-    const query = 'INSERT INTO category (userId, category, type) VALUES (?, ?, ?)';
-    pool.query(query, [userId, category, type], (err, results) => {
+    const query = 'INSERT INTO categories (UserId, Category, Type) VALUES (?, ?, ?)';
+    pool.query(query, [UserId, Category, Type], (err, results) => {
         if (err) {
             console.error("Erro ao cadastrar categoria:", err);
             return res.status(500).send("Erro ao cadastrar categoria");
@@ -51,9 +51,9 @@ router.post("/register", (req, res) => {
 //Rota para atualizar as categorias
 router.put("/update/:Id", (req, res) => {
     const { Id } = req.params;
-    const { category, type } = req.body;
-    const query = 'UPDATE category SET category = ?, type = ? WHERE Id = ?';
-    pool.query(query, [category, type, Id], (err, results) => {
+    const { Category, Type } = req.body;
+    const query = 'UPDATE categories SET Category = ?, Type = ? WHERE Id = ?';
+    pool.query(query, [Category, Type, Id], (err, results) => {
       if (err) {
         console.error("Erro ao atualizar categoria:", err);
         return res.status(500).send("Erro ao atualizar categoria");
@@ -66,14 +66,14 @@ router.put("/update/:Id", (req, res) => {
 //Rota para excluir as categorias
 router.delete("/delete/:Id", (req, res) => {
   const { Id } = req.params;
-  const { userId } = req.body;
+  const { UserId } = req.body;
   
-  if(!userId) {
+  if(!UserId) {
     return res.status(400).send("UserId é obrigatório!");
   }
 
-  const query = "DELETE FROM category WHERE Id = ? AND userId = ?";
-  pool.query(query, [Id, userId], (err, results) => {
+  const query = "DELETE FROM categories WHERE Id = ? AND UserId = ?";
+  pool.query(query, [Id, UserId], (err, results) => {
     if(err) {
       console.error("Erro ao excluir a categoria:", err);
       return res.status(500).send("Erro ao excluir categoria");

1b145feb63068f2995f8522b0526ee005d9828fc
Matteus <matteussantos30@gmail.com>
2024-10-24 10:37:16 -0300

    indented

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ require('dotenv').config();
 const app = express();
 const PORT = 3000;
 
+
 app.use(cors());
 app.use(express.json());
 
@@ -13,16 +14,17 @@ const userRoutes = require('./routes/userRoutes');
 const transactionsRoutes = require('./routes/transactionsRoutes');
 const categoryRoutes = require('./routes/categoryRoutes');
 
+
 app.use('/auth', authRoutes);
 app.use('/user', userRoutes);
 app.use('/transactions', transactionsRoutes);
 app.use('/category', categoryRoutes);
 
 
-
 app.get('/', (request, response) => {
     response.send("Hello World")
 });
+
 
 app.listen(PORT, () => {
         console.log(`Server is running on http://localhost:${PORT}`)

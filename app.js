c2a29638bd9e8b5a0ae2e6e9ee98f03fa9eb2a94
Matteus <matteussantos30@gmail.com>
2024-10-30 11:28:09 -0300

    calc Routes Added

--- app.js
+++ app.js
@@ -13,12 +13,14 @@ const authRoutes =require('./routes/authRoutes');
 const userRoutes = require('./routes/userRoutes');
 const transactionsRoutes = require('./routes/transactionsRoutes');
 const categoryRoutes = require('./routes/categoryRoutes');
+const calcRoutes = require('./routes/calcRoutes');
 
 
 app.use('/auth', authRoutes);
 app.use('/user', userRoutes);
 app.use('/transactions', transactionsRoutes);
 app.use('/category', categoryRoutes);
+app.use('/calc', calcRoutes);
 
 
 app.get('/', (request, response) => {
---
+++ routes/calcRoutes.js
@@ -0,0 +1,101 @@
+const express = require('express');
+const pool = require('../config/db');
+const authenticateToken = require('../middleware/authMiddleware');
+const router = express.Router();
+
+
+// Rota para buscar lançamentos da Entradas
+router.get("/inflows", authenticateToken, (req, res) => {
+  const UserId = req.user.id; 
+  console.log("Requisição recebida com UserId:", UserId); 
+
+  if (!UserId) {
+      return res.status(400).json({ message: "UserId é obrigatório." });
+  }
+
+  const query = `
+    SELECT Value, Date, Category
+    FROM Inflows 
+    WHERE UserId = ?`;
+  
+  pool.query(query, [UserId], (err, results) => {
+      if (err) {
+          console.error("Erro ao buscar os dados:", err);
+          return res.status(500).send("Erro ao buscar os dados");
+      }
+      res.status(200).json(results);
+  });
+});
+
+// Rota para buscar lançamentos da saídas do Debito
+router.get("/OutflowsDebit", authenticateToken, (req, res) => {
+    const UserId = req.user.id; 
+    console.log("Requisição recebida com UserId:", UserId); 
+  
+    if (!UserId) {
+        return res.status(400).json({ message: "UserId é obrigatório." });
+    }
+  
+    const query = `
+      SELECT Value, Date, Category
+      FROM DebitOutflows 
+      WHERE UserId = ?`;
+    
+    pool.query(query, [UserId], (err, results) => {
+        if (err) {
+            console.error("Erro ao buscar os dados:", err);
+            return res.status(500).send("Erro ao buscar os dados");
+        }
+        res.status(200).json(results);
+    });
+  });
+
+// Rota para buscar lançamentos de saídas do Crédito
+router.get("/OutflowsCredit", authenticateToken, (req, res) => {
+    const UserId = req.user.id; 
+    console.log("Requisição recebida com UserId:", UserId); 
+  
+    if (!UserId) {
+        return res.status(400).json({ message: "UserId é obrigatório." });
+    }
+  
+    const query = `
+      SELECT Value, Date, Category
+      FROM CreditOutflows 
+      WHERE UserId = ?`;
+    
+    pool.query(query, [UserId], (err, results) => {
+        if (err) {
+            console.error("Erro ao buscar os dados:", err);
+            return res.status(500).send("Erro ao buscar os dados");
+        }
+        res.status(200).json(results);
+    });
+  });
+
+  // Rota para buscar lançamentos das Saídas
+router.get("/Outflows", authenticateToken, (req, res) => {
+    const UserId = req.user.id; 
+    console.log("Requisição recebida com UserId:", UserId); 
+  
+    if (!UserId) {
+        return res.status(400).json({ message: "UserId é obrigatório." });
+    }
+  
+    const query = `
+      SELECT Value, Date, Category
+      FROM Outflows 
+      WHERE UserId = ?`;
+    
+    pool.query(query, [UserId], (err, results) => {
+        if (err) {
+            console.error("Erro ao buscar os dados:", err);
+            return res.status(500).send("Erro ao buscar os dados");
+        }
+        res.status(200).json(results);
+    });
+  });
+
+
+
+module.exports = router;

bd41ad355e03acf7eba251d8b1ba3e28f20f70f5
Matteus Abreu <matteussantos30@gmail.com>
2024-11-01 15:18:55 -0300

    Date format timezone calc

--- routes/calcRoutes.js
+++ routes/calcRoutes.js
@@ -3,42 +3,19 @@ const pool = require('../config/db');
 const authenticateToken = require('../middleware/authMiddleware');
 const router = express.Router();
 
-
 // Rota para buscar lançamentos da Entradas
 router.get("/Inflows", authenticateToken, (req, res) => {
-  const UserId = req.user.id; 
+    const UserId = req.user.id;
 
-  if (!UserId) {
-      return res.status(400).json({ message: "UserId é obrigatório." });
-  }
-
-  const query = `
-    SELECT Value, Date, Category
-    FROM Inflows 
-    WHERE UserId = ?`;
-  
-  pool.query(query, [UserId], (err, results) => {
-      if (err) {
-          console.error("Erro ao buscar os dados:", err);
-          return res.status(500).send("Erro ao buscar os dados");
-      }
-      res.status(200).json(results);
-  });
-});
-
-// Rota para buscar lançamentos da saídas do Debito
-router.get("/OutflowsDebit", authenticateToken, (req, res) => {
-    const UserId = req.user.id; 
-  
     if (!UserId) {
         return res.status(400).json({ message: "UserId é obrigatório." });
     }
-  
+
     const query = `
-      SELECT Value, Date, Category
-      FROM DebitOutflows 
-      WHERE UserId = ?`;
-    
+        SELECT Value, DATE_FORMAT(Date, '%Y-%m-%d') AS Date, Category
+        FROM Inflows 
+        WHERE UserId = ?`;
+
     pool.query(query, [UserId], (err, results) => {
         if (err) {
             console.error("Erro ao buscar os dados:", err);
@@ -46,21 +23,43 @@ router.get("/OutflowsDebit", authenticateToken, (req, res) => {
         }
         res.status(200).json(results);
     });
-  });
+});
+
+// Rota para buscar lançamentos das saídas do Débito
+router.get("/OutflowsDebit", authenticateToken, (req, res) => {
+    const UserId = req.user.id;
+
+    if (!UserId) {
+        return res.status(400).json({ message: "UserId é obrigatório." });
+    }
+
+    const query = `
+        SELECT Value, DATE_FORMAT(Date, '%Y-%m-%d') AS Date, Category
+        FROM DebitOutflows 
+        WHERE UserId = ?`;
+
+    pool.query(query, [UserId], (err, results) => {
+        if (err) {
+            console.error("Erro ao buscar os dados:", err);
+            return res.status(500).send("Erro ao buscar os dados");
+        }
+        res.status(200).json(results);
+    });
+});
 
 // Rota para buscar lançamentos de saídas do Crédito
 router.get("/OutflowsCredit", authenticateToken, (req, res) => {
-    const UserId = req.user.id; 
+    const UserId = req.user.id;
 
     if (!UserId) {
         return res.status(400).json({ message: "UserId é obrigatório." });
     }
-  
+
     const query = `
-      SELECT Value, Date, Category
-      FROM CreditOutflows 
-      WHERE UserId = ?`;
-    
+        SELECT Value, DATE_FORMAT(Date, '%Y-%m-%d') AS Date, Category
+        FROM CreditOutflows 
+        WHERE UserId = ?`;
+
     pool.query(query, [UserId], (err, results) => {
         if (err) {
             console.error("Erro ao buscar os dados:", err);
@@ -68,21 +67,21 @@ router.get("/OutflowsCredit", authenticateToken, (req, res) => {
         }
         res.status(200).json(results);
     });
-  });
+});
 
-  // Rota para buscar lançamentos das Saídas
+// Rota para buscar lançamentos das Saídas
 router.get("/Outflows", authenticateToken, (req, res) => {
-    const UserId = req.user.id; 
-  
+    const UserId = req.user.id;
+
     if (!UserId) {
         return res.status(400).json({ message: "UserId é obrigatório." });
     }
-  
+
     const query = `
-      SELECT Value, Date, Category
-      FROM Outflows 
-      WHERE UserId = ?`;
-    
+        SELECT Value, DATE_FORMAT(Date, '%Y-%m-%d') AS Date, Category
+        FROM Outflows 
+        WHERE UserId = ?`;
+
     pool.query(query, [UserId], (err, results) => {
         if (err) {
             console.error("Erro ao buscar os dados:", err);
@@ -90,8 +89,6 @@ router.get("/Outflows", authenticateToken, (req, res) => {
         }
         res.status(200).json(results);
     });
-  });
-
-
+});
 
 module.exports = router;

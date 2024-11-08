c9ec1420d9d659186679880b9bd2721c8e80b3fd
Matteus Abreu <matteussantos30@gmail.com>
2024-11-08 10:33:46 -0300

    reset pass route added

--- app.js
+++ app.js
@@ -6,9 +6,7 @@ const PORT = 3000;
 
 const allowedOrigin = process.env.FRONTEND_URL;
 
-app.use(cors({
-    origin: allowedOrigin
-}));
+app.use(cors());
 
 app.use(express.json());
 
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,13 +1,36 @@
+const db = require('../config/db'); 
+const jwt = require('jsonwebtoken');
 const express = require('express');
 const router = express.Router();
-const authenticateToken = require('../middleware/authMiddleware');
 
+const { JWT_SECRET } = process.env;
 
-// Rota para validar o token
-router.get('/validate-token', authenticateToken,(req, res) => {
-  res.status(200).json({ message: 'Token válido' });
+// Rota para gerar o link de redefinição
+router.post('/generate-reset-link', (req, res) => {
+  const { email } = req.body;
+
+ 
+  db.query('SELECT Id, Email, Username FROM users WHERE Email = ?', [email], (err, rows) => {
+    if (err) {
+      console.error('Erro ao consultar o banco de dados:', err);
+      return res.status(500).json({ message: 'Erro interno do servidor' });
+    }
+
+    if (rows.length === 0) {
+      return res.status(404).json({ message: 'Usuário não encontrado' });
+    }
+
+    const user = rows[0];
+
+    
+    const token = jwt.sign({ id: user.Id, email: user.Email }, JWT_SECRET, { expiresIn: '15m' });
+
+   
+    const resetLink = `${process.env.FRONTEND_URL}/reset-password?token=${token}`;
+
+  
+    res.status(200).json({ resetLink, username: user.Username });
+  });
 });
-
-
 
 module.exports = router;
